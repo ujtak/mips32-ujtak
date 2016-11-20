301b02e8f61eab42c10a16531d6128fcaaba418d
Takayuki Ujiie <kaftulal@hotmail.co.jp>
2016-11-20 23:32:27 +0900

    modified parameters

--- rtl/parameters.rb
+++ rtl/parameters.rb
@@ -1,1 +1,7 @@
-$dwidth = 32
+# ERB parameters
+
+# Verilog parameters
+$dwidth   = 32  # Data width
+$mdsize   = 8   # Data memory address width
+$rfsize   = 5   # Register file address size
+$aluwidth = 4   # ALU operation code width
