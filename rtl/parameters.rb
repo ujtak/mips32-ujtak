29c1417bcb30f3555ed6a367bd05c5b935a7591c
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2016-11-21 22:35:44 +0900

    modified tb for alu

--- rtl/parameters.rb
+++ rtl/parameters.rb
@@ -1,6 +1,7 @@
 # ERB parameters
 
 # Verilog parameters
+$step     = 10  # clock latency
 $dwidth   = 32  # Data width
 $dsize    = 8   # Data memory address width
 $isize    = 8   # Instruction memory address width
