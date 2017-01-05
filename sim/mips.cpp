a7ae9b7744a7e6e6ca9fbdcc67ebfac8a30965a1
Takayuki Ujiie <kaftulal@hotmail.co.jp>
2017-01-05 14:44:33 +0900

    sim: Add mips.cpp

---
+++ sim/mips.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class MIPS
+{
+  const int dwidth;
+  const int isize;
+  const int rsize;
+  const int dsize;
+  const int opwidth;
+
+}
+
+int main(void)
+{
+  return 0;
+}
